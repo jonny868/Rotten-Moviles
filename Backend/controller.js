5be54570bd8d4eb716bfd93133355188402e352d
JonnyDeev <jonnyp.dev@gmail.com>
2022-10-24 15:54:33 -0400

    DASHBOARD UPDATED / REGISTER IMPROVED

--- Backend/controller.js
+++ Backend/controller.js
@@ -12,17 +12,31 @@ ctrl.home = (req, res) => {
 //registro --> completo, falta añadir validaciones y seguridad
 
 ctrl.register = async (req, res) => {
-  const { email, password } = req.body;
-  const newUser = new User({ email, password });
+  const { email, password, username } = req.body;
+  const validateUsername = await User.findOne({ username: username });
+  const validateEmail = await User.findOne({ email: email });
+  //Validando que el usuario no esta usado
+  if (validateUsername) {
+    return res
+    .status(406)
+    .json({ ok: false, msg: "Username already taken, try a different one" });
+  }
+  //validando que el email no esta usado
+  if (validateEmail) {
+    return res
+    .status(406)
+    .json({ ok: false, msg: "Email already taken, try a different one" });
+  }
+  const newUser = new User({ email, password, username });
   await newUser.save();
   const token = await jwt.sign({ _id: newUser._id }, SECRET_KEY);
-  res.status(200).json({ token });
+  res.status(200).json({ token, username, email });
 };
 
 //login --> completo, falta añadir validaciones
 ctrl.login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
   if (!user) return res.status(401).send("Email or Password Incorrect");
   if (user.password !== password)
     return res.status(401).send("Email or Password Incorrect");
@@ -39,8 +53,9 @@ ctrl.dashboard = async (req, res) => {
       comments: [
         { username: "john1@gmail.com", comment: "it was good af", rate: 2 },
         { username: "john2@gmail.com", comment: "it was good af", rate: 1 },
-        { username: "john23@gmail.com", comment: "it was bad af", rate: 1 }],
-      rates:2,
+        { username: "john23@gmail.com", comment: "it was bad af", rate: 1 },
+      ],
+      rates: 2,
     },
     {
       name: "Malcom X",
@@ -48,7 +63,8 @@ ctrl.dashboard = async (req, res) => {
       comments: [
         { username: "john1@gmail.com", comment: "it was good af", rate: 2 },
         { username: "john2@gmail.com", comment: "it was good af", rate: 1 },
-        { username: "john23@gmail.com", comment: "it was bad af", rate: 1 }],
+        { username: "john23@gmail.com", comment: "it was bad af", rate: 1 },
+      ],
       rates: 1,
     },
     {
@@ -57,29 +73,30 @@ ctrl.dashboard = async (req, res) => {
       comments: [
         { username: "john1@gmail.com", comment: "it was good af", rate: 2 },
         { username: "john2@gmail.com", comment: "it was good af", rate: 1 },
-        { username: "john23@gmail.com", comment: "it was bad af", rate: 1 }],
+        { username: "john23@gmail.com", comment: "it was bad af", rate: 1 },
+      ],
       rates: " 3",
     },
   ]);
 };
 
-ctrl.profile = (req,res)=>{
-  res.send(req.userId)
-}
+ctrl.profile = (req, res) => {
+  res.send(req.userId);
+};
 
-ctrl.verifyToken = (req, res, next)  => {
-  if(!req.headers.authorization){
-    return res.status(401).send('Authorization Required')
+ctrl.verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send("Authorization Required");
   }
-  const token = req.headers.authorization.split(' ')[1]
-  if(token ==='null'){
-    return res.status(401).send('Authorization Required')
+  const token = req.headers.authorization.split(" ")[1];
+  if (token === "null") {
+    return res.status(401).send("Authorization Required");
   }
   //payload : data that can be passed trough different requests
-  const data = jwt.verify(token, SECRET_KEY)
+  const data = jwt.verify(token, SECRET_KEY);
   req.userId = data._id;
   next();
-  console.log(data)
-}
+  console.log(data);
+};
 
 module.exports = ctrl;
--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,8 +1,20 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-    email: String,
-    password: String,
+    username:{
+        type:String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type:String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type:String,
+       required: true
+    }
 }, {timestamps: true,
 });
 
